99bd0446216918352b28a37e1b66ba372eec06ad
Yehor Malakhov <egormalakhov2111@outlook.com>
2025-06-15 23:17:43 +0100

    much

--- app.py
+++ app.py
@@ -1,189 +1,47 @@
 import streamlit as st
-import geopandas as gpd
-from streamlit_folium import st_folium
-import folium
-from utils import *
+import streamlit as st
+import json
+from streamlit_lottie import st_lottie
+from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
-#st.logo("visual_assets\Logo_main.png", size="large")
-def main():
-    #st.set_page_config("WTF") # NICE TO SET
-    #st.title("Titles")
-    #st.caption("Subtitle")
 
-    data = gpd.read_file("data/data.shp")
-    data.columns = ['playground_quality', 
-                    'picnic_quality', 
-                    'parks_quality', 
-                    'sport_equipment_quality', 
-                    'sport_entities_quality',
-                    'hotels_quality', 
-                    'recycling_quality', 
-                    'restrooms_quality', 
-                    'funicular_quality', 
-                    'electrocar_quality',
-                    'tracks_quality', 
-                    'bicicle_quality', 
-                    'roads_quality', 
-                    'greens_quality',
-                    'dog_quality',
-                    'field_quality', 
-                    'metro_quality', 
-                    'train_quality', 
-                    'theatres_quality', 
-                    'museums_quality',
-                    'papelarias_quality', 
-                    'monuments_quality', 
-                    'malls_quality', 
-                    'mercados_quality', 
-                    'feiras_quality',
-                    'private_school_quality', 
-                    'public_school_quality', 
-                    'bombeiros_quality', 
-                    'private_hospital_quality', 
-                    'public_hospital_quality',
-                    'pharmacies_quality', 
-                    'health_center_quality', 
-                    'police_quality', 
-                    'public_kindergarten_quality', 
-                    'university_quality',
-                    'professionial_school_quality', 
-                    'private_kindergarten_quality', 
-                    'special_school_quality', 
-                    'Shape_Leng', 
-                    'Shape_Area',
-                    'geometry']
+st.sidebar.page_link("app.py", label="Home", icon=":material/home:")
+st.sidebar.page_link("pages/map.py", label="Build Your Map", icon=":material/public:")
 
 
-    list_names = ["parks_val", "greens_val", "museums_val", "monuments_val", "public_kindergarten_val", "private_kindergarten_val",
-                  "public_school_val", "private_school_val", "university_val", "special_school_val", "professionial_school_val",
-                  "sport_equipment_val", "playground_val", "dog_val", "field_val", "private_hospital_val", "public_hospital_val",
-                  "health_center_val", "pharmacies_val", "bombeiros_val", "police_val", "malls_val", "mercados_val", "feiras_val",
-                  "papelarias_val", "theatres_val", "picnic_val", "hotels_val", "metro_val", "train_val", "bicicle_val", "roads_val",
-                  "electrocar_val", "funicular_val", "tracks_val",
-                  
-                  "recycling_val", "restrooms_val"]
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+lottie_animation = load_lottiefile(r"visual_assets\ani1.json")
 
-    for val in list_names:
-        if val not in st.session_state:
-            st.session_state[val] = 0
 
-    if "mapped" not in st.session_state:
-            st.session_state["mapped"] = False
-    
-    topics = [
-              "Parks & Green Areas",
-              "Cultural & Historical Landmarks",
-              "Educational Institutions",
-              "Sports Facilities & Recreation",
-              "Healthcare Services",
-              "Public Safety & Law Enforcement",
-              "Retail & Grocery Stores",
-              "Leisure & Entertainment Venues",
-              "Housing Costs",
-              "Public Transport & Mobility"
-              ] 
+st.title("Mapping Your Ideal Place")
 
-    with st.sidebar:
-        selectbox = st.sidebar.selectbox("Topic", topics)
-        if selectbox == "Parks & Green Areas":
-            st.session_state["parks_val"] = st.number_input("Proximity to Parks", value=st.session_state["parks_val"])
-            st.session_state["greens_val"] = st.number_input("Proximity to Green Spaces", value=st.session_state["greens_val"])
-        elif selectbox == "Cultural & Historical Landmarks":
-            # TODO CENTER CULTURAL
-            st.session_state["museums_val"] = st.number_input("Proximity to Museums", value=st.session_state["museums_val"])
-            st.session_state["monuments_val"] = st.number_input("Proximity to National Monuments", 
-                                                                value=st.session_state["monuments_val"])
-        elif selectbox == "Educational Institutions":
-            # TODO Library 
-            st.session_state["public_kindergarten_val"] = st.number_input("Proximity to Public Kindergartens", 
-                                                                          value=st.session_state["public_kindergarten_val"])
-            st.session_state["private_kindergarten_val"] = st.number_input("Proximity to Private Kindergartens", 
-                                                                           value=st.session_state["private_kindergarten_val"])
-            st.session_state["public_school_val"] = st.number_input("Proximity to Public Schools", 
-                                                                    value=st.session_state["public_school_val"])
-            st.session_state["private_school_val"] = st.number_input("Proximity to Private Schools", 
-                                                                     value=st.session_state["private_school_val"])
-            st.session_state["university_val"] = st.number_input("Proximity to Universities", 
-                                                                 value=st.session_state["university_val"])
-            st.session_state["special_school_val"] = st.number_input("Proximity to Special-Needs Education Schools", 
-                                                                     value=st.session_state["special_school_val"])
-            st.session_state["professionial_school_val"] = st.number_input("Proximity to Professional Schools", 
-                                                                           value=st.session_state["professionial_school_val"])
-        elif selectbox == "Sports Facilities & Recreation":
-            #TODO pools? gyms?
-            st.session_state["sport_equipment_val"] = st.number_input("Proximity to Outdoor Gyms", 
-                                                                      value=st.session_state["sport_equipment_val"])
-            st.session_state["playground_val"] = st.number_input("Proximity to Playgrounds",
-                                                                  value=st.session_state["playground_val"])
-            st.session_state["dog_val"] = st.number_input("Proximity to Dog Playgrounds", value=st.session_state["dog_val"])
-            st.session_state["field_val"] = st.number_input("Proximity to Public Farms", value=st.session_state["field_val"])
-        elif selectbox == "Healthcare Services":
-            st.session_state["private_hospital_val"] = st.number_input("Proximity to Private Hospitals", 
-                                                                       value=st.session_state["private_hospital_val"])
-            st.session_state["public_hospital_val"] = st.number_input("Proximity to Public Hospitals", 
-                                                                      value=st.session_state["public_hospital_val"])
-            st.session_state["health_center_val"] = st.number_input("Proximity to Health Centers", 
-                                                                    value=st.session_state["health_center_val"])
-            st.session_state["pharmacies_val"] = st.number_input("Proximity to Pharmacies", value=st.session_state["pharmacies_val"])
-        elif selectbox == "Public Safety & Law Enforcement":
-            st.session_state["bombeiros_val"] = st.number_input("Proximity to Pharmacies", value=st.session_state["bombeiros_val"])
-            st.session_state["police_val"] = st.number_input("Proximity to Firefighters Stations", value=st.session_state["police_val"])
-        elif selectbox == "Retail & Grocery Stores":
-            st.session_state["malls_val"] = st.number_input("Proximity to Malls", value=st.session_state["malls_val"])
-            st.session_state["mercados_val"] = st.number_input("Proximity to Markets", value=st.session_state["mercados_val"])
-            st.session_state["feiras_val"] = st.number_input("Proximity to Fairs", value=st.session_state["feiras_val"])
-            st.session_state["papelarias_val"] = st.number_input("Proximity to Stationery Stores", 
-                                                                 value=st.session_state["papelarias_val"])
-        elif selectbox == "Leisure & Entertainment Venues":
-            # TODO cinemas?
-            st.session_state["theatres_val"] = st.number_input("Proximity to Theatres", value=st.session_state["theatres_val"])
-            st.session_state["picnic_val"] = st.number_input("Proximity to Picnic Spaces", value=st.session_state["picnic_val"])
-        elif selectbox == "Housing Costs":
-            # TODO rent, price
-            st.session_state["hotels_val"] = st.number_input("Proximity to Hotels", value=st.session_state["hotels_val"])
-        elif selectbox == "Public Transport & Mobility": 
-            st.session_state["metro_val"] = st.number_input("Proximity to Metro Stations", value=st.session_state["metro_val"])
-            st.session_state["train_val"] = st.number_input("Proximity to Train Stations", value=st.session_state["train_val"])
-            st.session_state["bicicle_val"] = st.number_input("Proximity to Bicicle Lanes", value=st.session_state["bicicle_val"])
-            st.session_state["roads_val"] = st.number_input("Proximity to Roads", value=st.session_state["roads_val"])
-            st.session_state["electrocar_val"] = st.number_input("Proximity to Electric Vehicle Charge Stations", 
-                                                                 value=st.session_state["electrocar_val"])
-            st.session_state["funicular_val"] = st.number_input("Proximity to Funiculars and Inclined Elevators", 
-                                                                 value=st.session_state["funicular_val"])
-            st.session_state["tracks_val"] = st.number_input("Remoteness from Train Tracks", value=st.session_state["tracks_val"])
+with st.container():
+    col1, col2, col6 = st.columns((4,1, 2))
+    with col1: 
 
-        with st.form(key="submit_form"):
-            st.session_state["submitted"] = st.form_submit_button(label="Submit when Finish Browsing")
-            if st.session_state["submitted"]:
-                for el in list_names:
-                    st.session_state[el.split("_")[0] + "_final"] = st.session_state["el"]
+        st.subheader("A Tool for Finding Your Best Location in Lisbon")
+        st.markdown("""Choosing where to live, visit, or rent has always been a complex decision. 
+                        
+Typically we seek locations that meet our personal preferences, such as proximity to amenities, access to public transport, green spaces, schools, and more. 
+                        
+Here you can define your most preferred points of interest and visualize the best locations in Lisbon based on these. 
+                        
+This tool identifies and ranks areas according to how well they satisfy the your criteria. 
+                        
+Locations with a higher concentration of your dersired Points of interest within a 5-minute walking radius are given the highest scores, while those farther away score lower.
+                    """)
+    with col6:
+        st_lottie(lottie_animation, speed=1, loop=True, quality="low", height=270, width=270)
 
-    if st.session_state["submitted"]: #or st.session_state["mapped"]:
-        st.session_state["mapped"] = True
-        st.session_state["submitted"] = False
-        tot = []
-        for val in list_names:
-            tot.append((val.split("_")[0] + "_quality", st.session_state[el.split("_")[0] + "_final"]))
-        st.session_state["trying"] = user_importance(data, tot)
-        st.session_state["map_base"] = st.session_state["trying"].explore(column='result', tiles='CartoDB dark_matter', cmap="RdYlGn", vmin=0, vmax=5,
-                                 location=[38.71, -9.05], zoom_start=10.5, scrollWheelZoom=False)
-        st_map = st_folium(st.session_state["map_base"], width=900, height=500)
-    elif st.session_state["mapped"]:
-        st.session_state["map_base"] = st.session_state["trying"].explore(column='result', tiles='CartoDB dark_matter', cmap="RdYlGn", vmin=0, vmax=5,
-                                 location=[38.71, -9.05], zoom_start=10.5, scrollWheelZoom=False)
-        st_map = st_folium(st.session_state["map_base"], width=900, height=500)
-    else:
-        map_base = folium.Map(location=[38.71, -9.05], zoom_start=10.5, scrollWheelZoom=False, tiles="CartoDB dark_matter")
-        st_map = st_folium(map_base, width=900, height=500)
-
-if __name__ == "__main__":
-    main()
+with st.container():
+    col3, col4, col5 = st.columns((1, 1, 1))
+    with col4:
+        Build_map= st.button("**Want to build your own map?! Build it here!**")
+        if Build_map:
+            st.switch_page("pages/map.py")
 
 
 
-############################################################################################
-[
-'recycling_quality', 
-'restrooms_quality', 
-]
--- utils.py
+++ utils.py
@@ -7,13 +7,28 @@ def user_importance(data, tups=[]):
     data_c["result"] = data.drop("geometry", axis=1).T.apply(lambda x: sum([x[tup[0]]*tup[1]/top for tup in tups]))
     data_c.sort_values(by="result", inplace=True)
     
-    breaks = jenkspy.jenks_breaks(data_c['result'], n_classes=6)
+    alpha = data_c['result'].nunique()
+    if alpha >= 6:
+        breaks = jenkspy.jenks_breaks(data_c['result'], n_classes=6)
+        labels = [0, 1, 2, 3, 4, 5]
+    else:
+        breaks = jenkspy.jenks_breaks(data_c['result'], n_classes=alpha)
+        labels = [0, 1, 2, 3, 4, 5][6-alpha:]
     breaks[0] = breaks[0] - 1
     data_c['result'] = pd.cut(data_c['result'],
                               bins=breaks,
-                              labels=[0, 1, 2, 3, 4, 5],
+                              labels=labels,
                               include_lowest=True)
 
     data_c = data_c.dissolve(by="result").reset_index()
 
     return data_c
+
+def splitter(txt):
+    lt = txt.split("_")
+    if len(lt) == 2:
+        return lt[0]
+    elif len(lt) == 3:
+        return lt[0] + "_" + lt[1]
+    else:
+        raise ValueError("Oh No! It seems Yehor is really in need of vacation!")
